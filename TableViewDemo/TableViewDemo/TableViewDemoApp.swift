5ee9a5a53d47849673eb7fd13934502aa4e971db
Andrew Hoang <andrewhoang7@gmail.com>
2022-03-12 21:23:11 -0600

    Project creation

---
+++ TableViewDemo/TableViewDemo/TableViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewDemoApp.swift
+//  TableViewDemo
+//
+//  Created by Andrew Hoang on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TableViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
